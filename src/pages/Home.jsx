6ab40611899ed468e1635581e88b6caee88bc530
Jahangir Hussen <ahmedjahangirid@gmail.com>
2025-12-07 15:03:00 +0600

    add Home page component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Slider from "../components/Slider";
+import PopularSection from "../components/PopularSection";
+import MeetOurVets from "../components/MeetOurVets";
+import WinterTips from "../components/WinterTips";
+import NEWSLETTER from "./NEWSLETTER";
+
+const Home = () => {
+  return (
+    <>
+      <Slider></Slider>
+      <PopularSection />
+      <MeetOurVets />
+      <WinterTips />
+      <NEWSLETTER />
+    </>
+  );
+};
+
+export default Home;
